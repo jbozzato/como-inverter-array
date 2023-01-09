ff526e8260eb21ddb8455a8e115460a4e994f7e0
joão bozzato <joaolmbozzato@gmail.com>
2023-01-09 10:44:39 -0300

    terceiro commit

---
+++ playlist-js/33-contar-elemento-tela/js/scripts.js
@@ -0,0 +1,8 @@
+// Contar elementos na tela
+var paragrafos = document.querySelectorAll("p")
+
+console.log(paragrafos)
+
+var titles = document.querySelectorAll(".title")
+
+console.log(titles.length)
